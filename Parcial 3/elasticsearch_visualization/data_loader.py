996b3620bb73dae64b0402a93d2c2dfb7cd61995
Eymard Guadalupe Cantú Campos <146991300+EymardCantu@users.noreply.github.com>
2025-05-14 01:07:54 -0500

    cambios en data_loader

--- Parcial 3/elasticsearch_visualization/data_loader.py
+++ Parcial 3/elasticsearch_visualization/data_loader.py
@@ -1,27 +1,32 @@
 import pandas as pd
 from elasticsearch import Elasticsearch
-import json
 
 # Ruta del archivo CSV
-file_path = r'C:\Documentos\Cómputo de alto desempeño\Github\Portafolio_computo\Parcial 3\elasticsearch_visualization\spotify_top_1000_tracks.csv'
+file_path = "/mnt/c/Documentos/Cómputo de alto desempeño/Github/Portafolio_computo/Parcial 3/elasticsearch_visualization/spotify_top_1000_tracks.csv"
 
-# Conectar a Elasticsearch
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+# Conectar a Elasticsearch con autenticación
+es = Elasticsearch(
+    "http://localhost:9200",
+    basic_auth=("elastic", "password") 
+)
 
 # Cargar el archivo CSV en un DataFrame
 try:
     data = pd.read_csv(file_path)
     print(f'Dataset cargado correctamente:\n{data.head()}')  # Muestra las primeras filas
 
-    # Crear un índice en Elasticsearch (si no existe)
-    if not es.indices.exists(index='spotify_tracks'):
-        es.indices.create(index='spotify_tracks')
+    # Crear un índice en Elasticsearch
+    if not es.indices.exists(index="spotify_tracks"):
+        es.indices.create(index="spotify_tracks", body={"settings": {"number_of_shards": 1}})
         print("Índice creado en Elasticsearch.")
+
 
     # Cargar cada fila del DataFrame en Elasticsearch
     for i, row in data.iterrows():
         doc = row.to_dict()  # Convierte la fila a un diccionario
-        es.index(index='spotify_tracks', id=i, document=doc)  # Indexa el documento
+        es.index(index="spotify_tracks", id=i, body={"doc": doc})
+
+
 
     print("Datos cargados en Elasticsearch exitosamente.")
 
